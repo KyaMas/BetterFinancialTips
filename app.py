dcacf082437124ba5d0f693dc6c83a5a366c835a
Pavankumar Sakhare <pavankumar.sakhare@gmail.com>
2024-11-28 17:14:26 -0400

    Graphs in Result Page

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 from flask_cors import CORS, cross_origin
 import base64
+import torch
+import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +19,8 @@ def home():
 @app.route('/predict', methods=['POST', 'OPTIONS'])
 @cross_origin(origins="http://127.0.0.1:5000", methods=["POST", "OPTIONS"])
 def upload_file():
+    print("TensorFlow installed:", tf.__version__)  # If this works, TensorFlow is installed.
+    print("PyTorch installed:", torch.__version__)  # If this works, PyTorch is installed.
     if request.method == 'OPTIONS':
         # Handle preflight request
         return jsonify({"message": "Preflight check passed"}), 200
@@ -43,9 +47,9 @@ def upload_file():
             return jsonify({"error": f"An error occurred while running scripts: {e}"}), 500
 
         # Encode images as base64 strings
-        with open("data/review_wordcloud.png", "rb") as image_file:
+        with open("templates/review_wordcloud.png", "rb") as image_file:
             wordcloud_base64 = base64.b64encode(image_file.read()).decode('utf-8')
-        with open("data/mds_visualization.png", "rb") as image_file:
+        with open("templates/mds_visualization.png", "rb") as image_file:
             mds_base64 = base64.b64encode(image_file.read()).decode('utf-8')
 
         # Return an array of both images as base64 strings
